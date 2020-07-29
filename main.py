f4d7a9930063b43bdc76eea15a6838b177a32654
BryceRyan <bryceryan1999@gmail.com>
2020-07-29 14:53:56 -0400

    Added some simpler ML using keras

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import flask
-import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras import Input
+from tensorflow.keras.layers import Dense
+import tensorflow.keras as keras
+import mysql.connector
+from mysql.connector import Error
+from sklearn.model_selection import train_test_split
 #Define the server object and stop it from caching for dev purposes
 app = flask.Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -12,40 +18,44 @@ def root():
 
 def trainML():
 
-    #Could *potentially* optimize these tuning params using k-fold cross validation.
-    learning_rate= .5
-    epochs = 10
-    neurons = 50
+    # Get data from DB
+    # The result of this should be as follows: 
+        # XData holds the information about the applicants, as well as the information about the job
+        # YData holds the job that the individuals actually took
+        # Each row corresponds to a customer-job match
+    try:
+        connection = mysql.connector.connect(host = "", database="", user="", password="")
+        sql_query = ""
+        cursor = connection.cursor()
+        cursor.execute(sql_query)
+        Xdata=cursor.fetchall()
+        sql_query = ""
+        cursor = connection.cursor()
+        cursor.execute(sql_query)
+        Ydata=cursor.fetchall()
+    except Error as e:
+        print("Error reading data", e)
 
-    #Change this once we can connect to the DB
-    features = 0
-    sample_size = 0
-    output_size = 0
+    X_train, X_test, y_train, y_test = train_test_split(Xdata, Ydata, test_size=.4)
+    features = len(Xdata[1])
 
-    # import data from DB here
-    x = None
-    y = None
+    x_in = Input(shape=(features,))
+    x = Dense(50, activation="relu")(x_in)
+    x_out = Dense(10, activation="sigmoid")(x)
 
-    # Define NN Model - X is the set of predictors for each job applicant, and y is the job they eventually selected
-    weight1 = tf.Variable(tf.random_normal[features, neurons], stddev = 0.03, name="W1")
-    bias1 = tf.Variable(tf.random_normal([neurons]), name="b1")
-    weight2 = tf.Variable(tf.random_normal([neurons, output_size], stddev=.03), name="W2")
-    bias2 = tf.Variable(tf.random_normal([output_size]), name="b2")
+    model = Model(inputs=x_in, outputs=x_out)
 
-    hidden_out = tf.add(tf.matmul(x, weight1), bias1)
-    hidden_out = tf.nn.relu(hidden_out)
+    model.compile(
+        loss= "binary_crossentropy",
+        optimizer = "adam",
+        metrics=["accuracy"],
+    )
 
-    # Sigmoid activation function so we can get a probability
-    output_layer=tf.math.sigmoid(tf.add(tf.matmul(hidden_out, weight2), bias2))
+    history = model.fit(x_train, y_train, batch_size=60, epochs=4, validation_split=.4)
 
-    output_clipped = tf.clip_by_value(output_layer, 1e-10, 0.9999999)
-    cross_entropy= -tf.reduce_mean(tf.reduce_sum(y*tf.log(output_clipped)+(1-y)*tf.log()))
+    test_scores = model.evaluate(x_test, y_test, verbose=2)
 
-
-    # Predictor set will include - all of the details about a job applicant, as well as all of the details about a given job.
-    # Put all of these into a NN, and see where it goes.
-    # Save the model in a .pckl file
-
+    model.save("/MySchedulingReplacement")
     return
 
 def runML():
